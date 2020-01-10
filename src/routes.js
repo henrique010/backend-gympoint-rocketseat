61620c2c23ab2c8a9f3fe66b7561673698728c3f
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-01-10 09:46:34 -0300

    user creation and editing, finished

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import UserController from './app/controllers/UserController';
 import Middleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -9,6 +10,9 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(Middleware);
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
 
